a2f2f17f4a21074a4bba5cfc0f930756bb403b50
Tsuri Yuma <yuma33t@gmail.com>
2023-09-11 16:04:45 +0900

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by 釣悠馬 on 2023/09/11.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
