f80d6a5aea630b2f19f82b25514d67232fd6c534
Tsuri Yuma <yuma33t@gmail.com>
2023-09-14 21:51:08 +0900

    2023/09/14

---
+++ Tutorial/MeetingView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("First")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Second")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
