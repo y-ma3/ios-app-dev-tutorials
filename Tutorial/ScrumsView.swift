05a919981b268e01a4b857f5c4900a5550a6de2c
Tsuri Yuma <yuma33t@gmail.com>
2023-10-17 23:14:23 +0900

    2023/10/17

--- Tutorial/ScrumsView.swift
+++ Tutorial/ScrumsView.swift
@@ -1,11 +1,16 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+//                .listRowBackground(scrum.theme.mainColor)
+        }
     }
 }
 
 #Preview {
-    ScrumsView()
+    ScrumsView(scrums: DailyScrum.sampleData)
 }
