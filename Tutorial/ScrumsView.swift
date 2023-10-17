0c0e305f6dafd4d04433d3f886f401767179ca18
Tsuri Yuma <yuma33t@gmail.com>
2023-10-17 23:08:43 +0900

    2023/10/17

---
+++ Tutorial/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Tutorial
+//
+//  Created by Yuma on 2023/10/17.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
