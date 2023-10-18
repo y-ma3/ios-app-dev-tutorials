427fc86299abc0396055ff1c6ae8fbd3bed86e25
Tsuri Yuma <yuma33t@gmail.com>
2023-10-18 20:58:28 +0900

    2023/10/18

--- Tutorial/ScrumsView.swift
+++ Tutorial/ScrumsView.swift
@@ -4,9 +4,15 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
-//                .listRowBackground(scrum.theme.mainColor)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                    //                .listRowBackground(scrum.theme.mainColor)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
         }
     }
 }
