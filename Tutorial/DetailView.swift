cfa4f50e2479552750031548e2a0336bcd1948a2
Tsuri Yuma <yuma33t@gmail.com>
2023-10-19 16:15:31 +0900

    2023/10/19

---
+++ Tutorial/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  Tutorial
+//
+//  Created by Yuma on 2023/10/19.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
