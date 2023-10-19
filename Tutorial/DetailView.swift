4fc7affc55bb6e691c46e231e548547862a67fe7
Tsuri Yuma <yuma33t@gmail.com>
2023-10-19 16:17:34 +0900

    2023/10/19

--- Tutorial/DetailView.swift
+++ Tutorial/DetailView.swift
@@ -1,18 +1,15 @@
-//
-//  DetailView.swift
-//  Tutorial
-//
-//  Created by Yuma on 2023/10/19.
-//
-
 import SwiftUI
 
 struct DetailView: View {
+    let scrum: DailyScrum
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    DetailView()
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
 }
