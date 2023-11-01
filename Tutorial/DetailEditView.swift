6bf41a40c3482fc21900b4d7812c76450aa2374e
Tsuri Yuma <yuma33t@gmail.com>
2023-11-01 22:37:28 +0900

    2023/11/01

--- Tutorial/DetailEditView.swift
+++ Tutorial/DetailEditView.swift
@@ -1,8 +1,14 @@
 import SwiftUI
 
 struct DetailEditView: View {
+    @State private var scrum = DailyScrum.emptyScrum
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+            }
+        }
     }
 }
 
