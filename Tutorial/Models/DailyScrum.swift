4ade601a3716dc5d0531a024b038b2a55076dfb2
Tsuri Yuma <yuma33t@gmail.com>
2023-11-01 22:33:16 +0900

    2023/11/01

--- Tutorial/Models/DailyScrum.swift
+++ Tutorial/Models/DailyScrum.swift
@@ -26,6 +26,10 @@ extension DailyScrum {
             self.name = name
         }
     }
+    
+    static var emptyScrum: DailyScrum {
+        DailyScrum(title: "", attendees: [], lengthInMinutes: 5, theme: .sky)
+    }
 }
 
 extension DailyScrum {
