a31db03ed8aa455a2b7c3733d3e7e2997387f52e
Tsuri Yuma <yuma33t@gmail.com>
2023-11-01 22:34:28 +0900

    2023/11/01

---
+++ Tutorial/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Tutorial
+//
+//  Created by Yuma on 2023/11/01.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
