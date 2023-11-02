ba6ce7253e36836a9e33a7732b68540eaf13c164
Tsuri Yuma <yuma33t@gmail.com>
2023-11-02 23:05:36 +0900

    2023/11/02

--- Tutorial/DetailEditView.swift
+++ Tutorial/DetailEditView.swift
@@ -7,6 +7,13 @@ struct DetailEditView: View {
         Form {
             Section(header: Text("Meeting Info")) {
                 TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
             }
         }
     }
