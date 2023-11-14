097b449a26bf883c8b4f849651665c08eb5f0460
Tsuri Yuma <yuma33t@gmail.com>
2023-11-14 11:24:04 +0900

    2023/11/14

--- Tutorial/ThemeView.swift
+++ Tutorial/ThemeView.swift
@@ -1,11 +1,13 @@
 import SwiftUI
 
 struct ThemeView: View {
+    let theme: Theme
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ThemeView()
+    ThemeView(theme: .buttercup)
 }
